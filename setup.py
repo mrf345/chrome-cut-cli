8f244613f85a337f1c033fb8317b8998972e22d0
mrf3 <mrf345@gmail.com>
2018-01-10 20:48:59 +0800

    adding setup

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+chrome-cut
+-------------
+
+Basic command line tool to scan, detect, stream and control chrome cast
+devices. \n
+"""
+from setuptools import setup
+
+
+setup(
+    name='chrome-cut',
+    version='0.2',
+    url='https://github.com/mrf345/chrome-cut-cli/',
+    download_url='https://github.com/mrf345/chrome-cut-cli/archive/0.2.tar.gz',
+    license='MPL 2.0',
+    author='Mohamed Feddad',
+    author_email='mrf345@gmail.com',
+    description='Command line tool to scan and control chrome cast devices.',
+    long_description=__doc__,
+    scripts=['chrome-cut'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests',
+        'click',
+        'netifaces'
+    ],
+    keywords=['chrome', 'cast', 'chromecast', 'control', 'monitor',
+              'restore', 'stream', 'scan'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Security', 'Topic :: System :: Monitoring',
+        'Topic :: Utilities'
+    ]
+)
